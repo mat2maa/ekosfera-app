228235306a0bf8658375414978c42626d3d256c8
Matthew Ager <matthew.a.ager@googlemail.com>
2014-05-13 23:29:20 +0200

    Tips

--- www/js/index.js
+++ www/js/index.js
@@ -7,6 +7,14 @@
     init: function () {
       Core.auth.requireSession();
       Core.index.bindEvents();
+      Core.index.updateUsefulTip(
+        {
+          onSuccess: Core.index.onSuccess,
+          onError: Core.index.onError,
+          onDenied: Core.index.onDenied,
+          onComplete: Core.index.onComplete
+        }
+      );
       Core.ui.showView();
     },
 
@@ -15,6 +23,62 @@
         Core.auth.logout();
         return false;
       });
+    },
+
+    updateUsefulTip: function (callback) {
+      var auth_token = Core.auth.authToken.get();
+
+      $.ajax({
+        type: "GET",
+        url: "http://" + host + "/api/update_current_useful_tip",
+        data: {
+          auth_token: auth_token
+        },
+        success: function (data) {
+          if (typeof callback.onSuccess == 'function') {
+            callback.onSuccess.call(this, data);
+          }
+        },
+        error: function (data, status) {
+          if (typeof callback.onError == 'function') {
+            if (data.status == '403') {
+              return callback.onDenied.call(this, data);
+            }
+            callback.onError.call(this, data);
+          }
+        },
+        complete: function (data) {
+          if (typeof callback.onComplete == 'function') {
+            callback.onComplete.call(this, data);
+          }
+        },
+        denied: function (data) {
+          if (typeof callback.onDenied == 'function') {
+            callback.onDenied.call(this, data);
+          }
+        }
+      });
+    },
+
+    onSuccess: function (data) {
+      console.log(data);
+      localStorage.setItem('useful_tip', JSON.stringify(data));
+    },
+
+    onError: function (data) {
+      console.log(data);
+      console.log("readyState: " + data.readyState);
+      console.log("responseText: " + data.responseText);
+      console.log("status: " + data.status);
+      console.log("statusText: " + data.statusText);
+    },
+
+    onDenied: function (data) {
+      console.log(data);
+    },
+
+    onComplete: function (data) {
+      console.log(data);
     }
 
   };
