be87c7ac842d66f6f8d88a633ec9299456035773
Matthew Ager <matthew.a.ager@googlemail.com>
2014-05-14 14:51:43 +0200

    Tips

--- www/js/core.js
+++ www/js/core.js
@@ -4,9 +4,22 @@
 
   Core = {
 
-
     init: function () {
       console.log("core/init");
+
+      Core.usefulTips.updateUsefulTip(
+        {
+          onSuccess: Core.usefulTips.onSuccess,
+          onError: Core.usefulTips.onError,
+          onDenied: Core.usefulTips.onDenied,
+          onComplete: Core.usefulTips.onComplete
+        }
+      );
+
+      Core.populateSettingsMenu();
+      Core.footerInit();
+      Core.bindEvents();
+
     },
 
     api: {
@@ -66,6 +79,160 @@
         });
 
       }
+
+    },
+
+    usefulTips: {
+
+      updateUsefulTip: function (callback) {
+        var auth_token = Core.auth.authToken.get(),
+          settings = JSON.parse(localStorage.getItem("ekosfera_settings"));
+
+        $.ajax({
+          type: "GET",
+          url: "http://" + host + "/api/update_current_useful_tip",
+          data: {
+            auth_token: auth_token,
+            settings: settings
+          },
+          success: function (data) {
+            if (typeof callback.onSuccess == 'function') {
+              callback.onSuccess.call(this, data);
+            }
+          },
+          error: function (data, status) {
+            if (typeof callback.onError == 'function') {
+              if (data.status == '403') {
+                return callback.onDenied.call(this, data);
+              }
+              callback.onError.call(this, data);
+            }
+          },
+          complete: function (data) {
+            if (typeof callback.onComplete == 'function') {
+              callback.onComplete.call(this, data);
+            }
+          },
+          denied: function (data) {
+            if (typeof callback.onDenied == 'function') {
+              callback.onDenied.call(this, data);
+            }
+          }
+        });
+      },
+
+      onSuccess: function (data) {
+        console.log(data);
+        localStorage.setItem('ekosfera_useful_tip', JSON.stringify(data));
+      },
+
+      onError: function (data) {
+        console.log(data);
+        console.log("readyState: " + data.readyState);
+        console.log("responseText: " + data.responseText);
+        console.log("status: " + data.status);
+        console.log("statusText: " + data.statusText);
+      },
+
+      onDenied: function (data) {
+        console.log(data);
+      },
+
+      onComplete: function (data) {
+        console.log(data);
+      },
+
+      refreshUsefulTip: function (callback) {
+        var auth_token = Core.auth.authToken.get(),
+          settings = JSON.parse(localStorage.getItem("ekosfera_settings"));
+
+        $.ajax({
+          type: "GET",
+          url: "http://" + host + "/api/refresh_current_useful_tip",
+          data: {
+            auth_token: auth_token,
+            settings: settings
+          },
+          success: function (data) {
+            console.log(data);
+            localStorage.setItem('ekosfera_useful_tip', JSON.stringify(data));
+            Core.footerInit();
+          },
+          error: function (data, status) {
+            console.log(data);
+            console.log("readyState: " + data.readyState);
+            console.log("responseText: " + data.responseText);
+            console.log("status: " + data.status);
+            console.log("statusText: " + data.statusText);
+          },
+          complete: function (data) {
+            console.log(data);
+          },
+          denied: function (data) {
+            console.log(data);
+          }
+        });
+      }
+
+    },
+
+    populateSettingsMenu: function () {
+
+      var settings = JSON.parse(localStorage.getItem("ekosfera_settings")),
+        $showTips = $("#show-useful-tips-checkbox"),
+        $tipsOptions = $(".useful-tip-option"),
+        settingsObject = {};
+
+      if (settings == null) {
+        $showTips.prop("checked", true).checkboxradio("refresh");
+        settingsObject["tipsOn"] = true;
+        $tipsOptions.each(function (index) {
+          $(this).prop("checked", true).checkboxradio("refresh");
+          settingsObject[index] = true;
+        });
+
+        localStorage.setItem("ekosfera_settings", JSON.stringify(settingsObject));
+      } else {
+        $showTips.prop("checked", settings["tipsOn"]).checkboxradio("refresh");
+        $tipsOptions.each(function (index) {
+          $(this).prop("checked", settings[index]).checkboxradio("refresh");
+        });
+      }
+
+      if ($showTips.prop("checked") == false) {
+        $tipsOptions.each(function () {
+          $(this).checkboxradio("option", "disabled", true).checkboxradio("refresh");
+        });
+      }
+
+    },
+
+    footerInit: function () {
+
+      $('.ui-footer').remove();
+
+      var footer = "",
+        settings = JSON.parse(localStorage.getItem("ekosfera_settings")),
+        tip = JSON.parse(localStorage.getItem("ekosfera_useful_tip"));
+
+      if (settings["tipsOn"] == true && tip != null) {
+        footer += "<div data-role='footer' data-position='fixed' data-tap-toggle='false' data-animate='true' role='contentinfo' data-theme='a' class='ui-footer ui-bar-inherit ui-footer-fixed slideup ui-bar-a'>";
+        footer += "<h4 class='useful-tip' role='heading' aria-level='1'>" + tip.useful_tip_category.name + "</h4>";
+        footer += "<hr/>";
+        footer += "<p class='useful-tip'>" + tip.text + "</p>";
+        footer += "<a href='#' class='ui-btn-right ui-btn ui-icon-refresh ui-btn-icon-notext ui-corner-all refresh-useful-tip' data-ajax='false' data-transition='none' data-role='button' role='button'>Refresh Useful Tip</a>";
+        footer += "</div>";
+        $("body").append(footer);
+      }
+
+    },
+
+    bindEvents: function () {
+
+      $(document).on("click", ".refresh-useful-tip", function(e) {
+        e.preventDefault();
+        Core.usefulTips.refreshUsefulTip();
+      });
 
     }
 
@@ -160,6 +327,24 @@
 
     hideView: function () {
       $("#content").hide();
+    }
+
+  };
+
+  window.Core = Core;
+
+})(jQuery);
+
+
+(function ($) {
+
+  var Core = window.Core || Core || {};
+
+  Core.tips = {
+
+    getUsefulTip: function () {
+      var tip = JSON.parse(localStorage.getItem("ekosfera_useful_tip")).text;
+      $(".useful-tip").html(tip);
     }
 
   };
--- www/js/settings.js
+++ www/js/settings.js
@@ -20,9 +20,10 @@
         var r = confirm("Are you sure you want to clear the cache?");
         if (r == true) {
           Object.keys(localStorage)
-            .forEach(function(key){
+            .forEach(function (key) {
               if (/^(ekosfera_)/.test(key) && (key != "ekosfera_auth_token" && key != "ekosfera_auth_token_expiration")) {
                 localStorage.removeItem(key);
+                location.reload();
               }
             });
         }
@@ -30,8 +31,61 @@
           return false;
         }
       });
-    }
 
+      var settings = JSON.parse(localStorage.getItem("ekosfera_settings")),
+        $showTips = $("#show-useful-tips-checkbox"),
+        $tipsOptions = $(".useful-tip-option");
+
+      $showTips.on("click", function () {
+        if ($(this).prop("checked") == false) {
+          $tipsOptions.each(function (index) {
+            $(this).checkboxradio("option", "disabled", true).checkboxradio("refresh");
+          });
+
+          settings["tipsOn"] = false;
+          localStorage.setItem("ekosfera_settings", JSON.stringify(settings));
+        } else {
+          $tipsOptions.each(function () {
+            $(this).checkboxradio("option", "disabled", false).checkboxradio("refresh");
+          });
+
+          if ($(".useful-tip-option:checked").length == 0) {
+            $tipsOptions.each(function (index) {
+              $(this).prop("checked", true).checkboxradio("refresh");
+              settings[index] = true;
+            });
+          }
+
+          settings["tipsOn"] = true;
+          localStorage.setItem("ekosfera_settings", JSON.stringify(settings));
+        }
+        Core.usefulTips.refreshUsefulTip();
+      });
+
+      $tipsOptions.each(function (index) {
+        $(this).on("click", function() {
+          if ($(this).prop("checked") == false) {
+            settings[index] = false;
+            localStorage.setItem("ekosfera_settings", JSON.stringify(settings));
+          } else {
+            settings[index] = true;
+            localStorage.setItem("ekosfera_settings", JSON.stringify(settings));
+          }
+
+          if ($(".useful-tip-option:checked").length == 0) {
+            $tipsOptions.each(function (index) {
+              $(this).checkboxradio("option", "disabled", true).checkboxradio("refresh");
+            });
+
+            $showTips.prop("checked", false).checkboxradio("refresh");
+            settings["tipsOn"] = false;
+            localStorage.setItem("ekosfera_settings", JSON.stringify(settings));
+          }
+          Core.usefulTips.refreshUsefulTip();
+        });
+      });
+
+    }
   };
 
   $(Core.settings.init);
